3316f50aa271ba0f880fcb9a7436c48245daaa91
0mgupta <omg90060@gmail.com>
2026-07-14 12:49:13 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,28 @@
+// require("dotenv").config();
+
+// const app = require("./src/app");
+// const connectDB = require("./src/config/database");
+
+// connectDB()
+// // invokeGeminiAi()
+
+
+// app.listen(3000, () => {
+//     console.log("server is running on port 3000");
+// });
+
 require("dotenv").config();
 
 const app = require("./src/app");
 const connectDB = require("./src/config/database");
 
-connectDB()
-// invokeGeminiAi()
+connectDB();
 
-
-app.listen(3000, () => {
+// Only listen locally, not on Vercel
+if (process.env.NODE_ENV !== "production") {
+  app.listen(3000, () => {
     console.log("server is running on port 3000");
-});
+  });
+}
+
+module.exports = app;
