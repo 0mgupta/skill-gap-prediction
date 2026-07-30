d2bf28277d1f25643504904634c4a15673cd8a95
0mgupta <omg90060@gmail.com>
2026-07-31 00:22:38 +0530

    Update user.model.js

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -9,7 +9,7 @@ const userSchema = new mongoose.Schema({
     email: {    
         type: String,
         unique : [true, "email already taken"],
-        required: true,
+        required: true, 
     },
     password: {
         type: String,
