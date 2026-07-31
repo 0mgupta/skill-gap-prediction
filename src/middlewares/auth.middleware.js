23fec107a13b2185a3705345b3d16747fd0db833
0mgupta <omg90060@gmail.com>
2026-08-01 01:07:18 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -6,7 +6,7 @@ async function authUser(req, res, next) {
         const token = req.cookies?.token;
 
         if (!token) {
-            return res.status(401).json({
+            return res.status(401).json({     
                 message: "Token not found"
             });
         }
