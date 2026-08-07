23b19d0361b6d953d09e6b5f44157a102aabbc2e
0mgupta <omg90060@gmail.com>
2026-08-07 23:48:09 +0530

    Update auth.middleware.js

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -8,7 +8,7 @@ async function authUser(req, res, next) {
         if (!token) {
             return res.status(401).json({     
                 message: "Token not found"
-            });
+            });  
         }  
 
         const blacklistedToken = await tokenBlacklistModel.findOne({
